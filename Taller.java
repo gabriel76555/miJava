4678832f1612cd64615e6b4383e0e878b929ac80
Gabriel <gabrielsegovia093@gmail.com>
2025-06-25 19:55:49 -0600

    mi primer cambio confirmado

---
+++ Taller.java
@@ -0,0 +1,12 @@
+public class Taller {
+    public static void main(String[]args){
+        
+        String nombre = "Estudiante";
+        int a = 5;
+        int b = 3;
+        int suma = a + b;
+
+        System.out.println("hola," + nombre + "!");
+        System.out.println("la suma de "+a+"+"+b+"es:"+ suma);
+    }
+}
