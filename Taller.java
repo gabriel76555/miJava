72d08754f362f255a3baba1fda3e78682b7c2a38
gabriel <gabrielsegovia093@gmail.com>
2025-06-25 20:15:10 -0600

    segundo cambio confirmado

--- Taller.java
+++ Taller.java
@@ -2,8 +2,8 @@ public class Taller {
     public static void main(String[]args){
         
         String nombre = "Estudiante";
-        int a = 5;
-        int b = 3;
+        int a = 10;
+        int b = 8;
         int suma = a + b;
 
         System.out.println("hola," + nombre + "!");
