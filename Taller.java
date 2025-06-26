bcb8597825bb2a95d4e32343fe92e966e50d9ce8
gabriel <gabrielsegovia093@gmail.com>
2025-06-25 20:42:36 -0600

    multiplicacion agregada

--- Taller.java
+++ Taller.java
@@ -5,8 +5,10 @@ public class Taller {
         int a = 10;
         int b = 8;
         int suma = a + b;
+        int multiplicacion = a * b;
 
         System.out.println("hola," + nombre + "!");
         System.out.println("la suma de "+a+"+"+b+"es:"+ suma);
+        System.out.println("la multiplicacion de"+a+"*"+b+"es:" +multiplicacion);
     }
 }
